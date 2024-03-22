2c2290c4082ad8442495c41300dc5f68bbd86fd5
Ronaldo S. <ronaldo.rs44@gmail.com>
2024-03-22 14:32:18 -0300

    feat: Improvements have been made to the workspace!

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from "tailwindcss";
 import { withTV } from "tailwind-variants/transformer";
 
+import preset from "@lib/tailwindcss";
+
 export default withTV<Config>({
 	content: ["index.html", "**/*.{js,ts,jsx,tsx}", "src/**/*.{js,ts,jsx,tsx}", "../../packages/**/**/*.{js,ts,jsx,tsx}"],
 	important: "html",
-	presets: [import("@lib/tailwindcss")],
+	presets: [preset],
 });
