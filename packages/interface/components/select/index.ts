3c496c021123e579df7c5d7d600bd250046ddb4e
Ronaldo S. <ronaldo.rs44@gmail.com>
2024-04-11 16:14:09 -0300

    refactor: The folder 'packages/ui' has been renamed to 'packages/interface'! Additionally, an implemented component of 'Select' has had its name changed to avoid conflicts

--- packages/interface/components/select/index.ts
+++ packages/interface/components/select/index.ts
@@ -3,6 +3,6 @@ import { Select } from "ui/components/select/component";
 export default Select;
 export * from "ui/components/select/class";
 export * from "ui/components/select/component";
-export * from "ui/components/select/container";
+export * from "ui/components/select/group";
 export * from "ui/components/select/opt-group";
 export * from "ui/components/select/option";
